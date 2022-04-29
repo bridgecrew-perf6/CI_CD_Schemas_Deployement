1590bc25f760b44b1c58bcdc42ac0b1f4c4b5b54
Venkata Kalpana Polu <VenkataKalpana.P@sstech.us>
2022-04-29 16:14:42 +0530

    ueba_components

--- migrations/V3.3.3__ueba_components.sql
+++ migrations/V3.3.3__ueba_components.sql
@@ -1,115 +1,5 @@
 use schema dev_demo_schema;
 
-create or replace view OUTLIERS_UEBA_GREATER_THAN_80(
-	EVENT_DATE,
-	ID,
-	X,
-	Y,
-	SCORE,
-	REPORT_NAME,
-	RESULT,
-	IS_THREAT,
-	PEERS,
-	TYPE
-) as
-select * from OUTLIERS_UEBA where score >= 80.0 ;
-
-
-create or replace view UEBA_DETAILS_VIEW(
-	EVENT_TIME,
-	IN_BYTES,
-	OUT_BYTES,
-	SRC_USER_NAME,
-	SRC_IP,
-	DST_IP,
-	SRC_PORT,
-	DST_PORT
-) as 
-  select event_time,IN_BYTES,out_bytes,src_user_name,src_ip,dst_ip,src_port,dst_port from wg_fw_networktraffic_odm
-;
-
-
-create or replace view UEBA_OUTLIERS_UEBA(
-	EVENT_DATE,
-	ID,
-	X,
-	Y,
-	SCORE,
-	REPORT_NAME,
-	TYPE
-) as
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
-coalesce(feature_agg:dwnld_bytes,0) ::bigint y,
-pas as score,'Bandwidth-Usage-by-Size' as Report_name,type from UEBA_BANDWIDTH_USAGE_BY_SIZE where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:if_upld_count,0) ::bigint x,coalesce(feature_agg:if_dwnld_count,0) ::bigint y,
-pas as score,'Bandwidth-Usage-by-Count' as Report_name,type from UEBA_BANDWIDTH_USAGE_BY_COUNT  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_count,0) ::bigint x,coalesce(feature_agg:download_count,0) ::bigint y,
-pas as score,'File-Downloads' as Report_name,type from UEBA_FILE_DOWNLOADS  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_port_count,0) ::bigint x,coalesce(feature_agg:src_count,0) ::bigint y,
-pas as score,'Bot-Attack' as Report_name,type from UEBA_BOT_ATTACK  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:privelege_total,0) ::bigint y,
-pas as score,'Privileged-Account-Usage' as Report_name,type from UEBA_PRIVILEGED_ACCOUNT_USAGE  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:end_point_remote_count,0) ::bigint x,coalesce(feature_agg:sent_bytes,0) ::bigint y,
-pas as score,'Endpoint-Indicators-of-Compromise' as Report_name,type from UEBA_END_POINT_INDICATORS  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:high_volume_count,0) ::bigint y,
-pas as score,'High-Volume-File-Access' as Report_name,type from UEBA_HIGH_VOLUME_FILE_ACCESS  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
-coalesce(feature_agg:distinct_domain_count,0) ::bigint y,
-pas as score,'Unusual-Domain' as Report_name,type from UEBA_UNUSUAL_DOMAIN  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:deviating_distance,0) ::bigint x,
-coalesce(feature_agg:no_of_sessions,0) ::bigint y,
-pas as score,'VPN-Conenctions' as Report_name,type from UEBA_VPN_SESSIONS  where score >=70.0 
-;
-
-
-create or replace view VW_NP_OUTLIERS_UEBA(
-	EVENT_DATE,
-	ID,
-	X,
-	Y,
-	SCORE,
-	REPORT_NAME,
-	TYPE
-) as
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
-coalesce(feature_agg:dwnld_bytes,0) ::bigint y,
-pas as score,'Bandwidth-Usage-by-Size' as Report_name,type from NP_UEBA_BANDWIDTH_USAGE_BY_SIZE where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:if_upld_count,0) ::bigint x,coalesce(feature_agg:if_dwnld_count,0) ::bigint y,
-pas as score,'Bandwidth-Usage-by-Count' as Report_name,type from NP_UEBA_BANDWIDTH_USAGE_BY_COUNT  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_count,0) ::bigint x,coalesce(feature_agg:download_count,0) ::bigint y,
-pas as score,'File-Downloads' as Report_name,type from NP_UEBA_FILE_DOWNLOADS  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_port_count,0) ::bigint x,coalesce(feature_agg:src_count,0) ::bigint y,
-pas as score,'Bot-Attack' as Report_name,type from NP_UEBA_BOT_ATTACK  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:end_point_remote_count,0) ::bigint x,coalesce(feature_agg:sent_bytes,0) ::bigint y,
-pas as score,'Endpoint-Indicators-of-Compromise' as Report_name,type from NP_UEBA_END_POINT_INDICATORS  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
-coalesce(feature_agg:distinct_domain_count,0) ::bigint y,
-pas as score,'Unusual-Domain' as Report_name,type from NP_UEBA_UNUSUAL_DOMAIN  where score >=70.0 
- UNION ALL 
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:deviating_distance,0) ::bigint x,
-coalesce(feature_agg:no_of_sessions,0) ::bigint y,
-pas as score,'VPN-Conenctions' as Report_name,type from NP_UEBA_VPN_SESSIONS  where score >=70.0
- UNION ALL 
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:high_volume_count,0) ::bigint y,
-pas as score,'High-Volume-File-Access' as Report_name,type from NP_UEBA_HIGH_VOLUME_FILE_ACCESS  where score >=70.0 
-UNION ALL
-select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:privelege_total,0) ::bigint y,
-pas as score,'Privileged-Account-Usage' as Report_name,type from NP_UEBA_PRIVILEGED_ACCOUNT_USAGE  where score >=70.0;
-
-
 
 create or replace TABLE NP_UEBA_BANDWIDTH_USAGE_BY_COUNT (
 	EVENT_START_DATE DATE,
@@ -280,4 +170,115 @@ create or replace TABLE WGTRAFFIC_TESTING_UEBA_BANDWIDTH_SCORE (
 	PAS FLOAT NOT NULL,
 	EXECUTION_TIMESTAMP NUMBER(38,0) NOT NULL
 );
+
+
+create or replace view OUTLIERS_UEBA_GREATER_THAN_80(
+	EVENT_DATE,
+	ID,
+	X,
+	Y,
+	SCORE,
+	REPORT_NAME,
+	RESULT,
+	IS_THREAT,
+	PEERS,
+	TYPE
+) as
+select * from OUTLIERS_UEBA where score >= 80.0 ;
+
+
+create or replace view UEBA_DETAILS_VIEW(
+	EVENT_TIME,
+	IN_BYTES,
+	OUT_BYTES,
+	SRC_USER_NAME,
+	SRC_IP,
+	DST_IP,
+	SRC_PORT,
+	DST_PORT
+) as 
+  select event_time,IN_BYTES,out_bytes,src_user_name,src_ip,dst_ip,src_port,dst_port from wg_fw_networktraffic_odm
+;
+
+
+create or replace view UEBA_OUTLIERS_UEBA(
+	EVENT_DATE,
+	ID,
+	X,
+	Y,
+	SCORE,
+	REPORT_NAME,
+	TYPE
+) as
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
+coalesce(feature_agg:dwnld_bytes,0) ::bigint y,
+pas as score,'Bandwidth-Usage-by-Size' as Report_name,type from UEBA_BANDWIDTH_USAGE_BY_SIZE where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:if_upld_count,0) ::bigint x,coalesce(feature_agg:if_dwnld_count,0) ::bigint y,
+pas as score,'Bandwidth-Usage-by-Count' as Report_name,type from UEBA_BANDWIDTH_USAGE_BY_COUNT  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_count,0) ::bigint x,coalesce(feature_agg:download_count,0) ::bigint y,
+pas as score,'File-Downloads' as Report_name,type from UEBA_FILE_DOWNLOADS  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_port_count,0) ::bigint x,coalesce(feature_agg:src_count,0) ::bigint y,
+pas as score,'Bot-Attack' as Report_name,type from UEBA_BOT_ATTACK  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:privelege_total,0) ::bigint y,
+pas as score,'Privileged-Account-Usage' as Report_name,type from UEBA_PRIVILEGED_ACCOUNT_USAGE  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:end_point_remote_count,0) ::bigint x,coalesce(feature_agg:sent_bytes,0) ::bigint y,
+pas as score,'Endpoint-Indicators-of-Compromise' as Report_name,type from UEBA_END_POINT_INDICATORS  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:high_volume_count,0) ::bigint y,
+pas as score,'High-Volume-File-Access' as Report_name,type from UEBA_HIGH_VOLUME_FILE_ACCESS  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
+coalesce(feature_agg:distinct_domain_count,0) ::bigint y,
+pas as score,'Unusual-Domain' as Report_name,type from UEBA_UNUSUAL_DOMAIN  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:deviating_distance,0) ::bigint x,
+coalesce(feature_agg:no_of_sessions,0) ::bigint y,
+pas as score,'VPN-Conenctions' as Report_name,type from UEBA_VPN_SESSIONS  where score >=70.0 
+;
+
+
+create or replace view VW_NP_OUTLIERS_UEBA(
+	EVENT_DATE,
+	ID,
+	X,
+	Y,
+	SCORE,
+	REPORT_NAME,
+	TYPE
+) as
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
+coalesce(feature_agg:dwnld_bytes,0) ::bigint y,
+pas as score,'Bandwidth-Usage-by-Size' as Report_name,type from NP_UEBA_BANDWIDTH_USAGE_BY_SIZE where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:if_upld_count,0) ::bigint x,coalesce(feature_agg:if_dwnld_count,0) ::bigint y,
+pas as score,'Bandwidth-Usage-by-Count' as Report_name,type from NP_UEBA_BANDWIDTH_USAGE_BY_COUNT  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_count,0) ::bigint x,coalesce(feature_agg:download_count,0) ::bigint y,
+pas as score,'File-Downloads' as Report_name,type from NP_UEBA_FILE_DOWNLOADS  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:dst_port_count,0) ::bigint x,coalesce(feature_agg:src_count,0) ::bigint y,
+pas as score,'Bot-Attack' as Report_name,type from NP_UEBA_BOT_ATTACK  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:end_point_remote_count,0) ::bigint x,coalesce(feature_agg:sent_bytes,0) ::bigint y,
+pas as score,'Endpoint-Indicators-of-Compromise' as Report_name,type from NP_UEBA_END_POINT_INDICATORS  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:upld_bytes,0) ::bigint x,
+coalesce(feature_agg:distinct_domain_count,0) ::bigint y,
+pas as score,'Unusual-Domain' as Report_name,type from NP_UEBA_UNUSUAL_DOMAIN  where score >=70.0 
+ UNION ALL 
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:deviating_distance,0) ::bigint x,
+coalesce(feature_agg:no_of_sessions,0) ::bigint y,
+pas as score,'VPN-Conenctions' as Report_name,type from NP_UEBA_VPN_SESSIONS  where score >=70.0
+ UNION ALL 
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:high_volume_count,0) ::bigint y,
+pas as score,'High-Volume-File-Access' as Report_name,type from NP_UEBA_HIGH_VOLUME_FILE_ACCESS  where score >=70.0 
+UNION ALL
+select event_start_date::date as event_date,name as id,coalesce(feature_agg:bandwidth,0) ::bigint x,coalesce(feature_agg:privelege_total,0) ::bigint y,
+pas as score,'Privileged-Account-Usage' as Report_name,type from NP_UEBA_PRIVILEGED_ACCOUNT_USAGE  where score >=70.0;
+
 
